631dd8b9ec3e739ebf9746f85cb15edbdf897707
jarenella <jamesa122333@gmail.com>
2022-12-19 16:36:47 -0800

    setup cards layout for portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function Portfolio() {
+
+  const cardWidth = {
+    width: '18rem',
+  }
+
   return (
-    <h1>test for portfolio</h1>
+    <div id="card-holder" className="d-flex justify-content-center">
+      <div className="card bg-secondary" style={cardWidth}>
+        <img className="card-img-top" src="..." alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
   );
 }
 
