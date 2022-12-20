6f129373b2fb7c24f74197ac6c4b9695de18dedc
jarenella <jamesa122333@gmail.com>
2022-12-19 16:23:29 -0800

    navbar buttons now highlight based on which one is active

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,18 @@ import Body from './Body';
 function Header() {
   const [currentPage, setCurrentPage] = useState('AboutMe');
 
+  const activeButtonCSS = "nav-link text-light bg-secondary rounded";
+  const nonActiveButtonCSS = "nav-link text-light rounded"
+  let aboutMeButtonCSS = '';
+  let portfolioButtonCSS = '';
+  let contactButtonCSS = '';
+  let resumeButtonCSS = '';
+  
+  currentPage === "AboutMe" ? aboutMeButtonCSS = activeButtonCSS : aboutMeButtonCSS = nonActiveButtonCSS;
+  currentPage === "Portfolio" ? portfolioButtonCSS = activeButtonCSS : portfolioButtonCSS = nonActiveButtonCSS;
+  currentPage === "Contact" ? contactButtonCSS = activeButtonCSS : contactButtonCSS = nonActiveButtonCSS;
+  currentPage === "Resume" ? resumeButtonCSS = activeButtonCSS : resumeButtonCSS = nonActiveButtonCSS;
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-dark">
@@ -15,22 +27,22 @@ function Header() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link text-light" href="#about-me" 
+              <a className={aboutMeButtonCSS} href="#about-me" 
               onClick={() => setCurrentPage('AboutMe')}
               >About Me</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-light" href="#portfolio"
+              <a className={portfolioButtonCSS} href="#portfolio"
               onClick={() => setCurrentPage('Portfolio')}
               >Portfolio</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-light" href="#contact"
+              <a className={contactButtonCSS} href="#contact"
               onClick={() => setCurrentPage('Contact')}
               >Contact</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-light" href="#resume"
+              <a className={resumeButtonCSS} href="#resume"
               onClick={() => setCurrentPage('Resume')}
               >Resume</a>
             </li>
