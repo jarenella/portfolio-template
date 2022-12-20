e617bdcbbab0f07c151c428ec37c38ce33e09970
jarenella <jamesa122333@gmail.com>
2022-12-20 13:54:18 -0800

    finished portfolio card links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,7 +14,8 @@ function Portfolio() {
           <div className="card-body">
             <h5 className="card-title">GitHelpNOW</h5>
             <p className="card-text">A group project inspired by the under-utilization of non-profit related APIs allowing users to both give and get help</p>
-            <a href="#" className="btn btn-primary">Check it out</a>
+            <a href="https://github.com/Renatatims/GitHelpNOW" className="btn btn-primary">Repo</a>
+            <a href="https://renatatims.github.io/GitHelpNOW/" className="btn btn-primary">Deployment</a>
           </div>
         </div>
       </div>
@@ -25,7 +26,7 @@ function Portfolio() {
           <div className="card-body">
             <h5 className="card-title">AccountabilliBuddies</h5>
             <p className="card-text">A group project allowing for users to make accounts and track their progress at goals of their choice</p>
-            <a href="#" className="btn btn-primary">Check it out</a>
+            <a href="https://github.com/mrcorbo/AccountabiliBuddies" className="btn btn-primary">Repo</a>
           </div>
         </div>
       </div>
@@ -36,7 +37,8 @@ function Portfolio() {
           <div className="card-body">
             <h5 className="card-title">Password Generator</h5>
             <p className="card-text">A front-end application that generates a cryptic password at random based off of user-input prompts</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <a href="https://github.com/jarenella/password-generator" className="btn btn-primary">Repo</a>
+            <a href="https://jarenella.github.io/password-generator/" className="btn btn-primary">Deployment</a>
           </div>
         </div>
       </div>
@@ -47,7 +49,8 @@ function Portfolio() {
           <div className="card-body">
             <h5 className="card-title">Weather Tracker</h5>
             <p className="card-text">This application allows users to add cities they choose to a list that allows them to track the current weather status there.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <a href="https://github.com/jarenella/weather-tracker" className="btn btn-primary">Repo</a>
+            <a href="https://jarenella.github.io/weather-tracker/" className="btn btn-primary">Deployment</a>
           </div>
         </div>
       </div>
@@ -57,8 +60,8 @@ function Portfolio() {
           <img className="card-img-top" src="..." alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">Readme Generator</h5>
-            <p className="card-text">A NodeJS app allowing a user to generate a readme for them based off of only command-line prompts. No need to type up your own markdown</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <p className="card-text">A NodeJS app allowing a user to generate a readme for them based off of only command-line prompts. No need to type up your own markdown. As a back-end application, this project has no deployment link</p>
+            <a href="https://github.com/jarenella/readme-generator" className="btn btn-primary">Repo</a>
           </div>
         </div>
       </div>
@@ -68,8 +71,8 @@ function Portfolio() {
           <img className="card-img-top" src="..." alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">Employee Tracker</h5>
-            <p className="card-text">This project allows you to store information about employees, managers, departments, salaries, etc. in a database and easily view the information through command line UI. Information can also be easily updated through the program</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <p className="card-text">This project allows you to store information about employees, managers, departments, salaries, etc. in a database and easily view the information through command line UI. Information can also be easily updated through the program. Because this project is back-end only, there is no deployment link</p>
+            <a href="https://github.com/jarenella/employee-tracker" className="btn btn-primary">Repo</a>
           </div>
         </div>
       </div>
