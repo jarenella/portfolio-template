4495d35c9c005bf46e49c1c0b6a161bfb7a19c4f
jarenella <jamesa122333@gmail.com>
2022-12-19 16:44:10 -0800

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,79 +10,19 @@ function Footer() {
                 <div className="row">
 
                     <div className="col-lg-3 col-md-6 mb-4 mb-md-0 text-light">
-                        <h5 className="text-uppercase">Links</h5>
-
-                        <ul className="list-unstyled mb-0">
-                            <li>
-                                <a href="#!" className="text-light">Link 1</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 2</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 3</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 4</a>
-                            </li>
-                        </ul>
+                        <h5 className="text-uppercase">LinkedIn</h5>
                     </div>
 
                     <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 className="text-uppercase mb-0 text-light">Links</h5>
-
-                        <ul className="list-unstyled">
-                            <li>
-                                <a href="#!" className="text-light">Link 1</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 2</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 3</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 4</a>
-                            </li>
-                        </ul>
+                        <h5 className="text-uppercase mb-0 text-light">GitHub</h5>
                     </div>
                     
                     <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 className="text-uppercase text-light">Links</h5>
-
-                        <ul className="list-unstyled mb-0">
-                            <li>
-                                <a href="#!" className="text-light">Link 1</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 2</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 3</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 4</a>
-                            </li>
-                        </ul>
+                        <h5 className="text-uppercase text-light">More</h5>
                     </div>
                     
                     <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 className="text-uppercase mb-0 text-light">Links</h5>
-
-                        <ul className="list-unstyled">
-                            <li>
-                                <a href="#!" className="text-light">Link 1</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 2</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 3</a>
-                            </li>
-                            <li>
-                                <a href="#!" className="text-light">Link 4</a>
-                            </li>
-                        </ul>
+                        <h5 className="text-uppercase mb-0 text-light">More</h5>
                     </div>
 
                 </div>
