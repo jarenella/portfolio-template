f5a96dcb770e3a09945d52e527d157fa43ff8a7a
jarenella <jamesa122333@gmail.com>
2022-12-20 14:15:06 -0800

    form html

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 
 function Contact() {
   return (
-    <h1>test for contact</h1>
+    <form>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">Your email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+      </div>
+
+      <div class="form-group">
+        <label for="exampleFormControlTextarea1">Your message here</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+    </form>
   );
 }
 
