091208a676ee6e9584d3ab5050691c791a8989db
jarenella <jamesa122333@gmail.com>
2022-12-19 16:01:42 -0800

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import React, { useState } from 'react';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,96 @@
+import React from 'react';
+
+
+function Footer() {
+    return (
+        <footer className="bg-dark text-center text-lg-start">
+
+            <div className="container p-4">
+
+                <div className="row">
+
+                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0 text-light">
+                        <h5 className="text-uppercase">Links</h5>
+
+                        <ul className="list-unstyled mb-0">
+                            <li>
+                                <a href="#!" className="text-light">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 className="text-uppercase mb-0 text-light">Links</h5>
+
+                        <ul className="list-unstyled">
+                            <li>
+                                <a href="#!" className="text-light">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 className="text-uppercase text-light">Links</h5>
+
+                        <ul className="list-unstyled mb-0">
+                            <li>
+                                <a href="#!" className="text-light">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 className="text-uppercase mb-0 text-light">Links</h5>
+
+                        <ul className="list-unstyled">
+                            <li>
+                                <a href="#!" className="text-light">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" className="text-light">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </footer>
+    );
+}
+
+export default Footer;
