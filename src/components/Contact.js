09c20e52f4734a24942549fefe33166e975d25a3
jarenella <jamesa122333@gmail.com>
2022-12-21 10:54:17 -0800

    added responsive messages that pop up when form fields are blank to tell users that they must not be blank

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,48 @@
 import React, { useState } from 'react';
 
 function Contact() {
+
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const inputChangeHandler = (element) => {
+    if (element.target.id === "FormControlInput1") {
+      const userInput = document.getElementById("FormControlInput1").value;
+      setEmail(userInput);
+    } else if (element.target.id === "formTextArea") {
+      const userInput = element.target.value;
+      setMessage(userInput);
+    }
+  }
+
   return (
     <form>
-      <div class="form-group">
-        <label for="exampleFormControlInput1">Your email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+      <div className="form-group">
+
+        {email === "" ? 
+        <div class="alert alert-danger" role="alert">
+          Email field cannot be blank
+        </div>
+        :
+          <p></p>
+        }
+
+        <label htmlFor="exampleFormControlInput1">Your email address</label>
+        <input name="email" onChange={inputChangeHandler} type="email" className="form-control" id="FormControlInput1" placeholder="name@example.com" />
       </div>
 
-      <div class="form-group">
-        <label for="exampleFormControlTextarea1">Your message here</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <div className="form-group">
+
+        {message === "" ?
+          <div class="alert alert-danger" role="alert">
+            Message field cannot be blank
+          </div>
+          :
+          <p></p>
+        }
+
+        <label htmlFor="exampleFormControlTextarea1">Your message here</label>
+        <textarea onChange={inputChangeHandler} className="form-control" id="formTextArea" rows="3"></textarea>
       </div>
     </form>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className="flex-container m-3 position-relative text-center text-light">
       <img className="img-fluid w-100" src="https://images.unsplash.com/photo-1671347020855-8f35d210ee9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" alt=""/>
-      <h2 class="hero-text position-absolute top-50 left-50">Simple solutions for complex problems</h2>
+      <h2 className="hero-text position-absolute top-50 left-50">Simple solutions for complex problems</h2>
     </div>
   );
 }
